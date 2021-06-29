cd70d6c1b92ac90f5ca127bfcf05f815bb7cafcc
faridfibrianto <faridfebrianto1997@gmail.com>
2021-06-29 15:43:41 +0700

    update

---
+++ test/test.py
@@ -0,0 +1,5 @@
+def gua_sebuah_fungsi():
+    print("gua dicall nih dari rapydscript")
+    return 'gua sebuah string dari rapydscript, mungkin gua dicall di js'
+
+module.exports = gua_sebuah_fungsi()
