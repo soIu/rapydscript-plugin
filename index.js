514e09fe4752de866664aec000d470417cb080c3
faridfibrianto <faridfebrianto1997@gmail.com>
2021-07-03 10:55:07 +0700

    add async await

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ var ρσ_iterator_symbol = (typeof Symbol === "function" && typeof Symbol.iterat
 var ρσ_kwargs_symbol = (typeof Symbol === "function") ? Symbol("kwargs-object") : "kwargs-object-Symbol-5d0927e5554349048cf0e3762a228256";
 var ρσ_cond_temp, ρσ_expr_temp, ρσ_last_exception;
 var ρσ_object_counter = 0;
+var async = asynchronous = (fn) => fn;
 `
 
 const rapydscript_curated_variables = JSON.parse("[\"ρσ_iterator_symbol\",\"ρσ_kwargs_symbol\",\"ρσ_cond_temp\",\"ρσ_expr_temp\",\"ρσ_last_exception\",\"ρσ_object_counter\",\"ρσ_len\",\"ρσ_bool\",\"ρσ_print\",\"ρσ_int\",\"ρσ_float\",\"ρσ_arraylike_creator\",\"options_object\",\"ρσ_id\",\"ρσ_dir\",\"ρσ_ord\",\"ρσ_chr\",\"ρσ_callable\",\"ρσ_bin\",\"ρσ_hex\",\"ρσ_enumerate\",\"ρσ_reversed\",\"ρσ_iter\",\"ρσ_range_next\",\"ρσ_range\",\"ρσ_getattr\",\"ρσ_setattr\",\"ρσ_hasattr\",\"ρσ_get_module\",\"ρσ_pow\",\"ρσ_type\",\"ρσ_divmod\",\"ρσ_max\",\"abs\",\"max\",\"min\",\"bool\",\"type\",\"float\",\"int\",\"arraylike\",\"ρσ_arraylike\",\"print\",\"id\",\"get_module\",\"pow\",\"divmod\",\"dir\",\"ord\",\"chr\",\"bin\",\"hex\",\"callable\",\"enumerate\",\"iter\",\"reversed\",\"len\",\"range\",\"getattr\",\"setattr\",\"hasattr\",\"ρσ_equals\",\"ρσ_not_equals\",\"equals\",\"ρσ_list_extend\",\"ρσ_list_index\",\"ρσ_list_pop\",\"ρσ_list_remove\",\"ρσ_list_to_string\",\"ρσ_list_insert\",\"ρσ_list_copy\",\"ρσ_list_clear\",\"ρσ_list_as_array\",\"ρσ_list_count\",\"ρσ_list_sort_key\",\"ρσ_list_sort_cmp\",\"ρσ_list_sort\",\"ρσ_list_concat\",\"ρσ_list_slice\",\"ρσ_list_iterator\",\"ρσ_list_len\",\"ρσ_list_contains\",\"ρσ_list_eq\",\"ρσ_list_decorate\",\"ρσ_list_constructor\",\"list\",\"list_wrap\",\"sorted\",\"ρσ_global_object_id\",\"ρσ_set_implementation\",\"ρσ_set_keyfor\",\"ρσ_set_polyfill\",\"ρσ_set\",\"ρσ_set_wrap\",\"set\",\"set_wrap\",\"ρσ_dict_implementation\",\"ρσ_dict_polyfill\",\"ρσ_dict\",\"ρσ_dict_wrap\",\"dict\",\"dict_wrap\",\"NameError\",\"Exception\",\"AttributeError\",\"IndexError\",\"KeyError\",\"ValueError\",\"UnicodeDecodeError\",\"AssertionError\",\"ZeroDivisionError\",\"ρσ_in\",\"ρσ_desugar_kwargs\",\"ρσ_exists\",\"ρσ_eslice\",\"ρσ_delslice\",\"ρσ_flatten\",\"ρσ_unpack_asarray\",\"ρσ_extends\",\"ρσ_Iterable\",\"ρσ_interpolate_kwargs\",\"ρσ_interpolate_kwargs_constructor\",\"ρσ_getitem\",\"ρσ_setitem\",\"ρσ_delitem\",\"ρσ_bound_index\",\"ρσ_splice\",\"ρσ_mixin\",\"ρσ_instanceof\",\"sum\",\"map\",\"filter\",\"zip\",\"any\",\"all\",\"decimal_sep\",\"define_str_func\",\"ρσ_unpack\",\"ρσ_orig_split\",\"ρσ_orig_replace\",\"ρσ_repr_js_builtin\",\"ρσ_html_element_to_string\",\"ρσ_repr\",\"ρσ_str\",\"str\",\"repr\"]");
 
-const module_variables = ['module', 'exports'].concat(rapydscript_curated_variables).join(', ')
+const module_variables = ['module', 'exports', 'async', 'asynchronous'].concat(rapydscript_curated_variables).join(', ')
 
 const getTemp = () => {
   if (tempCache) return tempCache;
@@ -43,8 +44,10 @@ const applyTransform = (p, t, state, value, calleeName, moduleString) => {
   const [temp, tempFile] = getTemp()
   const tempPath = temp.openSync({suffix: '.js'}).path
   const newTempPath = temp.openSync({suffix: '.js'}).path
-  require('child_process').execSync(process.execPath + ' ' + join(__dirname, 'node_modules/rapydscript-ng/bin/rapydscript') + ' compile -m ' + fullPath + ' -o ' + tempPath)
-  let code = require('fs').readFileSync(tempPath).toString()
+  let python_code = require('fs').readFileSync(fullPath).toString()
+  python_code = python_code.replace(/await /g, 'awaits + ')
+  require('child_process').execSync(process.execPath + ' ' + join(__dirname, 'node_modules/rapydscript-ng/bin/rapydscript') + ' compile -m  -o ' + tempPath, {input: python_code})
+  let code = require('fs').readFileSync(tempPath).toString().replace(/awaits \+ /g, 'void ')
   code = 'require("' + tempFile + '")(module, module.exports, function (' + module_variables + ') {\n' + code + '\n});'
   require('fs').writeFileSync(tempPath, code)
   moduleString.replaceWith(t.StringLiteral(tempPath))
@@ -59,6 +62,27 @@ const applyTransform = (p, t, state, value, calleeName, moduleString) => {
   });
 }
 
+function applyAsync(state, async_function) {
+  if (!transpiledCache[state.file.opts.filename]) return
+  if (!async_function) return
+  if (async_function.node && async_function.node.type === 'CallExpression') {
+    if (async_function.node.callee.type !== 'FunctionExpression') return
+    const first = async_function.node.callee.body.body[0]
+    if (first && first.type === 'VariableDeclaration' && first.declarations[0].id.name === 'ρσ_anonfunc') {
+      first.declarations[0].init.async = true
+    }
+    else {
+      async_function.node.callee.async = true
+    }
+  }
+  else if (async_function.node && async_function.node.type === 'FunctionExpression') async_function.node.async = true
+}
+
+function applyAwait(state, node) {
+  if (!transpiledCache[state.file.opts.filename]) return
+  node.operator = 'await ' //For some reason if we don't add a space it will concatenate the Unary Expression with the argument
+}
+
 function transformImportsInline ({ types: t }) {
   return {
     visitor: {
@@ -67,7 +91,9 @@ function transformImportsInline ({ types: t }) {
       },
       CallExpression (p, state) {
         const callee = p.get('callee')
-        if (!callee.isIdentifier() || !callee.equals('name', 'require')) {
+        if (!callee.isIdentifier()) return
+        if (!callee.equals('name', 'require')) {
+          if (callee.equals('name', 'async') || callee.equals('name', 'asynchronous')) applyAsync(state, p.get('arguments')[0])
           return
         }
 
@@ -77,6 +103,10 @@ function transformImportsInline ({ types: t }) {
         }
 
         applyTransform(p, t, state, arg.node.value, 'require', arg)
+      },
+      UnaryExpression(p, state) {
+        if (p.node.operator !== 'void') return
+        applyAwait(state, p.node)
       }
     }
   }
--- test/test.py
+++ test/test.py
@@ -5,4 +5,11 @@ def gua_sebuah_fungsi():
     console.log(require('./test2.js'))
     return 'gua sebuah string dari rapydscript, mungkin gua dicall di js hehe'
 
+@async
+def do_something_async():
+    dirs = await [1, 2, 3] #require('fs').promises.readdir('./')
+    return dirs
+
+do_something_async().then(console.log)
+
 module.exports = gua_sebuah_fungsi()
